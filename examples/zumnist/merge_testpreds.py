2d82a79213f967e10fc870ee9669ea939d4428d2
Paul Mineiro <paul.mineiro@gmail.com>
2015-01-05 17:31:57 -0800

    inspired by admm, 98.6%

--- examples/zumnist/merge_testpreds.py
+++ examples/zumnist/merge_testpreds.py
@@ -4,9 +4,10 @@ import h5py
 import numpy as np
 import sys
 
-f = h5py.File(sys.argv[1],"w")
+numnets = int(sys.argv[1]);
+f = h5py.File(sys.argv[2],"w")
 
-data = f.create_dataset("data", (60000,1,28,28), dtype='f')
+data = f.create_dataset("data", (60000*numnets,1,28,28), dtype='f')
 
 mnist = open('../../data/mnist/train-images-idx3-ubyte', 'r');
 magic_s = mnist.read(4)
@@ -16,20 +17,18 @@ n_columns_s = mnist.read(4)
 
 n=0
 for chunk in iter(lambda: mnist.read(28*28), ''):
-  data[n] = np.array([float(ord(c)) for c in list(chunk)]).reshape(1,28,28)
-  n=n+1
+  for i in range(0, numnets):
+    data[n] = np.array([float(ord(c))/256.0 for c in list(chunk)]).reshape(1,28,28)
+    n=n+1
 
-labels = f.create_dataset("label", (60000,10), dtype='f')
+labels = f.create_dataset("label", (60000*numnets,1), dtype='f')
 
 n=0
 epsilon=1e-6
 for line in sys.stdin:
-  values = [epsilon+(1-epsilon)*float(digit) for digit in line.split()]
-  valuesreshape=np.array(values).reshape(-1,10)
-  nummodels=valuesreshape.shape[0]
-  valuesarray = np.exp(np.sum(np.log(valuesreshape),axis=0)/nummodels)
-  valuesarray = valuesarray / valuesarray.sum()
-  labels[n]=valuesarray
-  n=n+1
+  values = [float(digit) for digit in line.split()]
+  for i in range(0, numnets):
+    labels[n]=values[i]
+    n=n+1
 
 f.close()
