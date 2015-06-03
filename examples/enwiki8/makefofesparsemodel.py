9c8dde47897876b0358a922dc210c6fc44af5add
Paul Mineiro <paul.mineiro@gmail.com>
2015-06-02 20:17:44 -0700

    sparse on cpu faster (but less than 2x faster)

--- examples/enwiki8/makefofesparsemodel.py
+++ examples/enwiki8/makefofesparsemodel.py
@@ -1,21 +1,32 @@
 #! /usr/bin/env python
 
 import caffe
+import math
 import numpy as np
 import sys
 import time
+from scipy.sparse import csr_matrix
+
+import pprint
 
 alpha=0.9
 eta=1.0
 decay=0.99999
 
+#np.seterr(all='raise')
+
 np.random.seed(8675309)
 
 vocabsize=80000
-batchsize=500 
+embeddingsize=200
+batchsize=250 
 
 invocabsize=vocabsize+2
 outvocabsize=vocabsize+1
+
+preembed=np.zeros(shape=(invocabsize,embeddingsize),dtype='f')
+preembed[:]=np.random.standard_normal(size=(invocabsize,embeddingsize))
+embedding=math.sqrt(embeddingsize)*np.linalg.qr(preembed)[0]
 
 net = caffe.Net(sys.argv[1])
 net.set_mode_gpu()
@@ -30,9 +41,13 @@ for (layer,momlayer) in zip(net.layers,momnet.layers):
     blob.data[:]=0.01*np.random.standard_normal(size=blob.data.shape)
     momblob.data[:]=np.zeros(blob.data.shape,dtype='f')
 
+momembeddiff=np.zeros(shape=(invocabsize,embeddingsize),dtype='f')
+
 f=open(sys.argv[2],'r')
 
-data=np.zeros((batchsize,1,1,invocabsize),dtype='f')
+row=[]
+col=[]
+value=[]
 labels=np.zeros(batchsize,dtype='f')
 bindex=0
 start=time.time()
@@ -53,23 +68,44 @@ for ii in range(10):
   
       for word in yx[1:]:
           iv=[subword for subword in word.split(':')]
-          data[bindex,0,0,int(iv[0])-1]=float(iv[1])
+          row.append(bindex)
+          col.append(int(iv[0])-1)
+          value.append(float(iv[1]))
       
       bindex=bindex+1
   
       if bindex >= batchsize:
+          sd=csr_matrix((value, (row, col)), shape=(batchsize,invocabsize), dtype='f')
+          data=sd.dot(embedding).reshape(batchsize,1,1,embeddingsize)
           net.set_input_arrays(data,labels)
           res=net.forward()
           sumloss+=res['loss'][0,0,0,0]
           sumsinceloss+=res['loss'][0,0,0,0]
           net.backward()
+          data_diff=net.blobs['data'].diff.reshape(batchsize,embeddingsize)
+
+          # y = W x
+          # y_k = \sum_l W_kl x_l
+          # df/dW_ij = \sum_k df/dy_k dy_k/dW_ij
+          #          = \sum_k df/dy_k (\sum_l 1_{i=k} 1_{j=l} x_l)
+          #          = \sum_k df/dy_k 1_{i=k} x_j
+          #          = df/dy_i x_j
+          # df/dW    = (df/dy)*x'
+
+          sdtransdiff=sd.transpose().tocsr().dot(data_diff);
+
+          momembeddiff=alpha*momembeddiff+eta*sdtransdiff;
+          embedding=embedding-momembeddiff
+
           for (layer,momlayer) in zip(net.layers,momnet.layers):
-            # TODO: skip input layer (?)
             for (blob,momblob) in zip(layer.blobs,momlayer.blobs):
-              momblob.data[:]=alpha*momblob.data[:]+eta*blob.diff;
+              momblob.data[:]=alpha*momblob.data[:]+eta*blob.diff
               blob.data[:]-=momblob.data[:]
+
           eta=eta*decay
-          data[:]=0
+          value=[]
+          row=[]
+          col=[]
           labels[:]=0
           bindex=0
           numupdates=numupdates+1
@@ -85,21 +121,14 @@ for ii in range(10):
 now=time.time()
 print "%11.4f\t%11.4f\t%11.4f\t%11u\t%11.6g"%(now-start,sumloss/numupdates,sumsinceloss/numsinceupdates,numupdates*batchsize,eta)
 
-#     delta t         average           since         example        learning   
-#                        loss            last         counter            rate   
-#      2.0940         11.2902         11.2902             500         0.99999   
-#      3.8769         11.2853         11.2804            1000         0.99998   
-#      7.5326         11.2677         11.2501            2000         0.99996   
-#     14.6913         11.2055         11.1433            4000         0.99992   
-#     28.9187         10.8767         10.5479            8000         0.99984   
-#     56.1917         10.1833          9.4900           16000         0.99968   
-#    110.5309          9.5003          8.8172           32000         0.99936   
-#    219.2732          8.8847          8.2691           64000        0.998721   
-#    436.7229          8.4796          8.0744          128000        0.997443   
-#    873.3536          8.1329          7.7863          256000        0.994893   
-#   1841.8988          7.8306          7.5284          512000        0.989812   
-#   3934.7942          7.5553          7.2799         1024000        0.979728   
-#   8271.9772          7.3096          7.0639         2048000        0.959867   
-#  16917.2846          7.0906          6.8715         4096000        0.921345   
-#  34494.6173          6.8883          6.6861         8192000        0.848877
-#  70322.8506          6.6791          6.4699        16384000        0.720592
+#    delta t         average           since         example        learning
+#                       loss            last         counter            rate
+#     1.0651         11.2877         11.2877             250         0.99999
+#     2.0385         11.2835         11.2793             500         0.99998
+#     3.9110         11.2670         11.2505            1000         0.99996
+#     7.6542         11.2011         11.1351            2000         0.99992
+#    14.8690         10.8806         10.5601            4000         0.99984
+#    28.4091         10.1851          9.4896            8000         0.99968
+#    55.4836          9.4967          8.8084           16000         0.99936
+#   109.7036          8.9067          8.3167           32000        0.998721
+#...
