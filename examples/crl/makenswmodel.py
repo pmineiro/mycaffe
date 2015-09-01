1f0a129730be5cb5ac2b8bf09bd3e3887478e33b
Paul Mineiro <paul-github@mineiro.com>
2015-09-01 12:59:50 -0700

    crl tweak

--- examples/crl/makenswmodel.py
+++ examples/crl/makenswmodel.py
@@ -11,7 +11,7 @@ import pdb
 np.random.seed(8675309)
 
 alpha=0.9
-eta=1e-3
+eta=1e-2
 etadecay=0.999992
 weightdecay=1e-3
 kappa=0.25
@@ -23,25 +23,19 @@ maximpweight=10
 # UGH ... so much for DRY
 
 lrs=dict()
-lrs[('ip0',0)]=40
-lrs[('ip0',1)]=40
-lrs[('bn0',0)]=40
-lrs[('bn0',1)]=40
-lrs[('ip1',0)]=30
-lrs[('ip1',1)]=30
-lrs[('bn1',0)]=30
-lrs[('bn1',1)]=30
-lrs[('ip2',0)]=20
-lrs[('ip2',1)]=20
-lrs[('bn2',0)]=20
-lrs[('bn2',1)]=20
+lrs[('ip0',0)]=4
+lrs[('ip0',1)]=4
+lrs[('ip1',0)]=3
+lrs[('ip1',1)]=3
+lrs[('ip2',0)]=2
+lrs[('ip2',1)]=2
 lrs[('treated_prediction',0)]=1
 lrs[('treated_prediction',1)]=0    # no bias weight allowed (?)
 lrs[('untreated_prediction',0)]=1
 lrs[('untreated_prediction',1)]=0  # no bias weight allowed (?)
-lrs[('treatment_ip3',0)]=20
-lrs[('treatment_ip3',1)]=20
-lrs[('treatment_prediction',0)]=20
+lrs[('treatment_ip3',0)]=2
+lrs[('treatment_ip3',1)]=2
+lrs[('treatment_prediction',0)]=2
 lrs[('treatment_prediction',1)]=0  # ... treatment is apriori balanced (?)
 
 def nicetime(dt):
@@ -77,13 +71,16 @@ momnet.set_mode_cpu()
 momnet.set_phase_train()
 
 for (layer,momlayer) in zip(net.layers,momnet.layers):
+  blobnum=0
   for (blob,momblob) in zip(layer.blobs,momlayer.blobs):
-    #blob.data[:]=0.01*np.random.standard_normal(size=blob.data.shape)
+    if blobnum == 0:
+      fan_in = float(blob.count) / float(blob.num)
+      scale = math.sqrt(3.0/fan_in)
+      blob.data[:]=np.random.uniform(low=-scale,high=scale,size=blob.data.shape)
+    else:
+      blob.data[:]=0
     momblob.data[:]=np.zeros(blob.data.shape,dtype='f')
-
-net.params['treated_prediction'][1].data[:]=0
-net.params['untreated_prediction'][1].data[:]=0
-net.params['treatment_prediction'][1].data[:]=0
+    blobnum=blobnum+1
 
 # nsw (treated or control)
 with open(sys.argv[2],'r') as f:
@@ -143,8 +140,8 @@ sys.stderr=os.fdopen(sys.stderr.fileno(), 'w', 0)
 
 print "mean population diff: %g"%(np.mean(ynsw,dtype='d')-np.mean(ypsid,dtype='d'))
 
-print "%7s %7s %7s %7s %7s %6s %7s %10s %9s"%("delta t","outcome","outcome","treated","imp","ateott","example","learning","gradient")
-print "%7s %7s %7s %7s %7s %6s %7s %10s %9s"%("","loss T","loss U","loss","weight","","counter","rate","scale")
+print "%7s %7s %7s %7s %7s %7s %7s %10s %9s"%("delta t","outcome","outcome","treated","imp","ateott","example","learning","gradient")
+print "%7s %7s %7s %7s %7s %7s %7s %10s %9s"%("","loss T","loss U","loss","weight","","counter","rate","scale")
 
 maxminlambda=maxlambda
 totaldata=xnsw.shape[0]+xpsid.shape[0]
@@ -216,7 +213,7 @@ for ii in range(maxiter):
         now=time.time()
         dt=float(now-start)
 
-        print "%7s %7.3f %7.3f %7.3f %7.3f %6.3f %7s %10.5g %9.3e"%(
+        print "%7s %7.3f %7.3f %7.3f %7.3f %7.4f %7s %10.5g %9.3e"%(
             nicetime(dt),
             sumsincetreatedloss/numsinceupdates,
             sumsinceuntreatedloss/numsinceupdates,
@@ -238,7 +235,7 @@ net.save(sys.argv[4])
 now=time.time()
 dt=float(now-start)
 
-print "%7s %7.3f %7.3f %7.3f %7.3f %6.3f %7s %10.5g %9.3e"%(
+print "%7s %7.3f %7.3f %7.3f %7.3f %7.4f %7s %10.5g %9.3e"%(
     nicetime(dt),
     sumsincetreatedloss/numsinceupdates,
     sumsinceuntreatedloss/numsinceupdates,
@@ -251,23 +248,24 @@ print "%7s %7.3f %7.3f %7.3f %7.3f %6.3f %7s %10.5g %9.3e"%(
 
 # GLOG_minloglevel=5 lambda=0.0 PYTHONPATH=../../python python makenswmodel.py nsw_train nsw_control.txt psid_controls.txt nswplacebomodel
 # mean population diff: -1.64639
-# delta t outcome outcome treated     imp ateott example   learning  gradient
-#          loss T  loss U    loss  weight        counter       rate     scale
-#    32ms   0.584   7.122   0.693   1.000 -0.011     850 0.00099999 0.000e+00
-#    59ms   0.581   7.442   0.693   1.000 -0.040      1K 0.00099998 0.000e+00
-#    95ms   0.567   7.690   0.693   1.000 -0.153      3K 0.00099997 0.000e+00
-#   156ms   0.513   5.861   0.693   1.000 -0.518      6K 0.00099994 0.000e+00
-#   280ms   0.391   3.458   0.693   1.000 -1.496     13K 0.00099987 0.000e+00
-#   519ms   0.336   2.471   0.693   1.000 -1.776     27K 0.00099974 0.000e+00
-#   921ms   0.327   1.761   0.688   0.997 -0.876     54K 0.00099949 0.000e+00
-#  1.760s   0.311   1.162   0.594   0.932 -0.417    108K 0.00099898 0.000e+00
-#  3.346s   0.296   1.068   0.428   0.950 -0.243    217K 0.00099795 0.000e+00
-#  6.605s   0.290   1.060   0.392   0.941 -0.254    435K 0.00099591 0.000e+00
-# 13.237s   0.273   1.031   0.387   0.932 -0.182    870K 0.00099184 0.000e+00
-# 26.385s   0.256   1.002   0.356   0.900 -0.078      1M 0.00098375 0.000e+00
-# 52.987s   0.232   0.931   0.268   0.814 -0.151      3M 0.00096776 0.000e+00
-#  1.779m   0.192   0.821   0.189   0.757 -0.004      6M 0.00093657 0.000e+00
-#  3.551m   0.141   0.622   0.126   0.666  0.100     13M 0.00087715 0.000e+00
-#  7.127m   0.109   0.458   0.057   0.574  0.005     27M  0.0007694 0.000e+00
-# 14.398m   0.086   0.330   0.032   0.539 -0.013     55M 0.00059198 0.000e+00
-# 28.744m   0.064   0.231   0.019   0.523 -0.009    111M 0.00035043 0.000e+00
+# delta t outcome outcome treated     imp  ateott example   learning  gradient
+#          loss T  loss U    loss  weight         counter       rate     scale
+#    32ms   0.586   6.655   0.693   1.000 -0.0093     850  0.0099999 0.000e+00
+#    54ms   0.585   6.910   0.693   1.000 -0.0301      1K  0.0099998 0.000e+00
+#   102ms   0.577   7.197   0.693   1.000 -0.1075      3K  0.0099997 0.000e+00
+#   170ms   0.529   5.943   0.693   1.000 -0.3976      6K  0.0099994 0.000e+00
+#   286ms   0.380   3.552   0.693   1.000 -1.8953     13K  0.0099987 0.000e+00
+#   496ms   0.348   2.722   0.693   1.000 -1.2550     27K  0.0099974 0.000e+00
+#   907ms   0.327   1.717   0.685   0.993 -0.4703     54K  0.0099949 0.000e+00
+#  1.728s   0.304   1.138   0.562   0.919 -0.3270    108K  0.0099898 0.000e+00
+#  3.358s   0.297   1.070   0.428   0.947 -0.3001    217K  0.0099795 0.000e+00
+#  6.616s   0.292   1.058   0.413   0.948 -0.2282    435K  0.0099591 0.000e+00
+# 13.244s   0.273   1.028   0.388   0.920 -0.3314    870K  0.0099184 0.000e+00
+# 26.990s   0.253   0.999   0.373   0.913 -0.0049      1M  0.0098375 0.000e+00
+# 54.392s   0.221   0.916   0.298   0.850  0.0384      3M  0.0096776 0.000e+00
+#  1.823m   0.175   0.769   0.213   0.755  0.0773      6M  0.0093657 0.000e+00
+#  3.637m   0.132   0.597   0.133   0.662 -0.0117     13M  0.0087715 0.000e+00
+#  7.415m   0.086   0.408   0.058   0.570  0.1779     27M   0.007694 0.000e+00
+# 14.703m   0.059   0.249   0.029   0.533  0.0247     55M  0.0059198 0.000e+00
+# 29.056m   0.048   0.165   0.022   0.524  0.0076    111M  0.0035043 0.000e+00
+# 44.453m   0.044   0.126   0.021   0.522  0.0076    170M   0.002019 0.000e+00
