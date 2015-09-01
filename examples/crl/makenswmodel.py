aad114b2cd84cb8d91006bcfa4b36cedc40a12a0
Paul Mineiro <paul-github@mineiro.com>
2015-09-01 08:36:41 -0700

    crl stuff

--- examples/crl/makenswmodel.py
+++ examples/crl/makenswmodel.py
@@ -13,21 +13,28 @@ np.random.seed(8675309)
 alpha=0.9
 eta=1e-3
 etadecay=0.999992
-weightdecay=1e-5
+weightdecay=1e-3
 kappa=0.25
 maxiter=200000
 maxlambda=float(os.environ['lambda'])
 lambdadecay=0.99999
+maximpweight=10
 
 # UGH ... so much for DRY
 
 lrs=dict()
 lrs[('ip0',0)]=40
 lrs[('ip0',1)]=40
+lrs[('bn0',0)]=40
+lrs[('bn0',1)]=40
 lrs[('ip1',0)]=30
 lrs[('ip1',1)]=30
+lrs[('bn1',0)]=30
+lrs[('bn1',1)]=30
 lrs[('ip2',0)]=20
 lrs[('ip2',1)]=20
+lrs[('bn2',0)]=20
+lrs[('bn2',1)]=20
 lrs[('treated_prediction',0)]=1
 lrs[('treated_prediction',1)]=0    # no bias weight allowed (?)
 lrs[('untreated_prediction',0)]=1
@@ -71,12 +78,12 @@ momnet.set_phase_train()
 
 for (layer,momlayer) in zip(net.layers,momnet.layers):
   for (blob,momblob) in zip(layer.blobs,momlayer.blobs):
-    blob.data[:]=0.01*np.random.standard_normal(size=blob.data.shape)
+    #blob.data[:]=0.01*np.random.standard_normal(size=blob.data.shape)
     momblob.data[:]=np.zeros(blob.data.shape,dtype='f')
 
 net.params['treated_prediction'][1].data[:]=0
 net.params['untreated_prediction'][1].data[:]=0
-#net.params['treatment_prediction'][1].data[:]=0
+net.params['treatment_prediction'][1].data[:]=0
 
 # nsw (treated or control)
 with open(sys.argv[2],'r') as f:
@@ -161,14 +168,15 @@ for ii in range(maxiter):
     sumsincetreatmentloss+=treatmentloss
 
     # TODO: importance weight, xrm, etc.
-    net.blobs['treated_lossdetail'].data[:,0,0,0]=np.fmin(10,np.divide(0.5,net.blobs['treatment_probability'].data[0:halfbatchsize,1,0,0]))
-    net.blobs['untreated_lossdetail'].data[:,0,0,0]=np.fmin(10,np.divide(0.5,net.blobs['treatment_probability'].data[-halfbatchsize:,0,0,0]))
-    impweight=np.mean(np.vstack((net.blobs['treated_lossdetail'].data,
-                                 net.blobs['untreated_lossdetail'].data)),
-                      dtype='d')
-    normweight=impweight
-    net.blobs['treated_lossdetail'].data[:,0,0,0]=np.divide(net.blobs['treated_lossdetail'].data[:,0,0,0],impweight)
-    net.blobs['untreated_lossdetail'].data[:,0,0,0]=np.divide(net.blobs['untreated_lossdetail'].data[:,0,0,0],impweight)
+    net.blobs['treated_lossdetail'].data[:,0,0,0]=np.fmin(maximpweight,np.divide(0.5,1e-6+net.blobs['treatment_probability'].data[0:halfbatchsize,1,0,0]))
+    treatedimpweight=np.mean(net.blobs['treated_lossdetail'].data,dtype='d')
+    net.blobs['untreated_lossdetail'].data[:,0,0,0]=np.fmin(maximpweight,np.divide(0.5,1e-6+net.blobs['treatment_probability'].data[-halfbatchsize:,0,0,0]))
+    untreatedimpweight=np.mean(net.blobs['untreated_lossdetail'].data,dtype='d')
+    
+    net.blobs['treated_lossdetail'].data[:,0,0,0]=np.divide(net.blobs['treated_lossdetail'].data[:,0,0,0],treatedimpweight)
+    net.blobs['untreated_lossdetail'].data[:,0,0,0]=np.divide(net.blobs['untreated_lossdetail'].data[:,0,0,0],untreatedimpweight)
+
+    impweight=0.5*(treatedimpweight+untreatedimpweight)
     sumimpweight+=impweight
     sumsinceimpweight+=impweight
 
@@ -241,50 +249,25 @@ print "%7s %7.3f %7.3f %7.3f %7.3f %6.3f %7s %10.5g %9.3e"%(
     eta,
     maxlambda-maxminlambda)
 
-# GLOG_minloglevel=5 PYTHONPATH=../../python python makenswmodel.py nsw_train nsw_control.txt psid_controls.txt nswplacebomodel   
+# GLOG_minloglevel=5 lambda=0.0 PYTHONPATH=../../python python makenswmodel.py nsw_train nsw_control.txt psid_controls.txt nswplacebomodel
 # mean population diff: -1.64639
-# delta t outcome outcome treated    ate example   learning  gradient
-#          loss T  loss U    loss        counter       rate     scale
-#   108ms   0.586   6.997   0.693 -0.002     850       0.05 0.000e+00
-#   158ms   0.586   6.408   0.693 -0.005      1K       0.05 0.000e+00
-#   204ms   0.585   6.967   0.693 -0.016      3K       0.05 0.000e+00
-#   282ms   0.574   6.316   0.693 -0.222      6K       0.05 0.000e+00
-#   427ms   0.415   4.095   0.693 -1.477     13K   0.049999 0.000e+00
-#   695ms   0.349   2.828   0.693 -1.356     27K   0.049998 0.000e+00
-#  1.243s   0.329   2.281   0.693 -0.486     54K   0.049997 0.000e+00
-#  2.472s   0.312   1.338   0.545 -0.454    108K   0.049994 0.000e+00
-#  4.906s   0.298   1.096   0.420 -0.198    217K   0.049987 0.000e+00
-# 10.089s   0.289   1.083   0.417 -0.380    435K   0.049974 0.000e+00
-# 20.263s   0.267   1.015   0.393 -0.338    870K   0.049949 0.000e+00
-# 38.714s   0.220   0.889   0.300 -0.144      1M   0.049898 0.000e+00
-#  1.274m   0.149   0.675   0.178 -0.268      3M   0.049796 0.000e+00
-#  2.527m   0.099   0.452   0.072 -0.364      6M   0.049592 0.000e+00
-#  5.071m   0.081   0.301   0.039 -0.467     13M   0.049187 0.000e+00
-# 10.282m   0.064   0.215   0.026 -0.423     27M   0.048388 0.000e+00
-# 21.199m   0.050   0.164   0.024 -0.476     55M   0.046828 0.000e+00
-# 43.855m   0.044   0.131   0.025 -0.285    111M   0.043858 0.000e+00
-#  1.530h   0.040   0.098   0.027 -0.539    222M    0.03847 0.000e+00
-
-#mean population diff: -1.64639
-#delta t outcome outcome treated ateott example   learning  gradient
-#         loss T  loss U    loss        counter       rate     scale
-#  126ms   0.586   6.997   0.693 -0.000     850  0.0099999 2.000e-06
-#  152ms   0.586   6.414   0.693 -0.001      1K  0.0099998 4.000e-06
-#  194ms   0.586   6.993   0.693 -0.003      3K  0.0099997 8.000e-06
-#  268ms   0.585   6.568   0.693 -0.009      6K  0.0099994 1.600e-05
-#  415ms   0.581   7.085   0.693 -0.070     13K  0.0099987 3.200e-05
-#  703ms   0.455   4.596   0.693 -2.161     27K  0.0099974 6.399e-05
-# 1.270s   0.337   2.647   0.693 -1.762     54K  0.0099949 1.280e-04
-# 2.300s   0.326   2.385   0.693 -1.626    108K  0.0099898 2.558e-04
-# 5.241s   0.306   1.223   0.624 -0.367    217K  0.0099795 5.113e-04
-# 9.830s   0.298   1.091   0.418 -0.376    435K  0.0099591 1.021e-03
-#19.508s   0.295   1.060   0.404 -0.364    870K  0.0099184 2.038e-03
-#38.044s   0.289   1.019   0.403 -0.313      1M  0.0098375 4.054e-03
-# 1.264m   0.269   0.953   0.414 -0.214      3M  0.0096776 8.027e-03
-# 2.561m   0.203   0.796   0.449 -0.414      6M  0.0093657 1.573e-02
-# 5.062m   0.131   0.533   0.258 -0.248     13M  0.0087715 3.022e-02
-#10.415m   0.083   0.316   0.131 -0.172     27M   0.007694 5.588e-02
-#21.543m   0.061   0.182   0.175 -0.475     55M  0.0059198 9.615e-02
-#44.368m   0.051   0.112   0.252 -0.641    111M  0.0035043 1.461e-01
-# 1.501h   0.045   0.074   0.343 -0.669    222M   0.001228 1.855e-01
-
+# delta t outcome outcome treated     imp ateott example   learning  gradient
+#          loss T  loss U    loss  weight        counter       rate     scale
+#    32ms   0.584   7.122   0.693   1.000 -0.011     850 0.00099999 0.000e+00
+#    59ms   0.581   7.442   0.693   1.000 -0.040      1K 0.00099998 0.000e+00
+#    95ms   0.567   7.690   0.693   1.000 -0.153      3K 0.00099997 0.000e+00
+#   156ms   0.513   5.861   0.693   1.000 -0.518      6K 0.00099994 0.000e+00
+#   280ms   0.391   3.458   0.693   1.000 -1.496     13K 0.00099987 0.000e+00
+#   519ms   0.336   2.471   0.693   1.000 -1.776     27K 0.00099974 0.000e+00
+#   921ms   0.327   1.761   0.688   0.997 -0.876     54K 0.00099949 0.000e+00
+#  1.760s   0.311   1.162   0.594   0.932 -0.417    108K 0.00099898 0.000e+00
+#  3.346s   0.296   1.068   0.428   0.950 -0.243    217K 0.00099795 0.000e+00
+#  6.605s   0.290   1.060   0.392   0.941 -0.254    435K 0.00099591 0.000e+00
+# 13.237s   0.273   1.031   0.387   0.932 -0.182    870K 0.00099184 0.000e+00
+# 26.385s   0.256   1.002   0.356   0.900 -0.078      1M 0.00098375 0.000e+00
+# 52.987s   0.232   0.931   0.268   0.814 -0.151      3M 0.00096776 0.000e+00
+#  1.779m   0.192   0.821   0.189   0.757 -0.004      6M 0.00093657 0.000e+00
+#  3.551m   0.141   0.622   0.126   0.666  0.100     13M 0.00087715 0.000e+00
+#  7.127m   0.109   0.458   0.057   0.574  0.005     27M  0.0007694 0.000e+00
+# 14.398m   0.086   0.330   0.032   0.539 -0.013     55M 0.00059198 0.000e+00
+# 28.744m   0.064   0.231   0.019   0.523 -0.009    111M 0.00035043 0.000e+00
