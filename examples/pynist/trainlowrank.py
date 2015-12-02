62db5353995d284ffee8d55500a5c557ead2cd37
Paul Mineiro <paul-github@mineiro.com>
2015-12-02 10:24:39 -0800

    low rank gradients

--- examples/pynist/trainlowrank.py
+++ examples/pynist/trainlowrank.py
@@ -5,12 +5,15 @@ import gzip
 import math
 import numpy as np
 import random
+import scipy.sparse.linalg
 import struct
 import os
 import sys
 import time
 
 from Pretty import nicetime, nicecount
+
+import pdb
 
 batchsize=64
 eta=1e-6
@@ -24,6 +27,26 @@ np.random.seed(8675309)
 
 sys.stdout=os.fdopen(sys.stdout.fileno(), 'w', 0) 
 sys.stderr=os.fdopen(sys.stderr.fileno(), 'w', 0) 
+
+def mysvds(A,k):
+  p=10
+  (n,d)=A.shape
+  # A = U S V.T
+  # A.T A = V S^2 V.T
+  # U = A V Sinv
+  Omega=np.random.randn(d,k+p)
+  Z=A.T.dot(A.dot(Omega))
+  Omega,_=np.linalg.qr(Z)
+  Z=A.T.dot(A.dot(Omega))
+  V,s,_=np.linalg.svd(Z,full_matrices=False)
+  V=V[:,0:k]
+  s=s[0:k:]**0.5
+  U=A.dot(V.dot(np.diag([1.0/sk if sk > 1e-6 else 0 for sk in s])))
+#  megu,megs,megv=scipy.sparse.linalg.svds(A,1)
+#  V=V.T
+#  print np.max(np.abs(V[0:1,:]-megv))
+#  print np.max(np.abs(U[:,0:1]-megu))
+  return (U,s,V)
 
 def net():
   n = caffe.NetSpec()
@@ -65,14 +88,17 @@ with open(protofilename,'w') as f:
 caffe.set_mode_cpu()
 net = caffe.Net(protofilename, caffe.TRAIN)
 momnet = caffe.Net(protofilename, caffe.TRAIN)
+resnet = caffe.Net(protofilename, caffe.TRAIN)
 
-for (layer,momlayer) in zip(net.layers,momnet.layers):
-  for blobnum,(blob,momblob) in enumerate(zip(layer.blobs,momlayer.blobs)):
+for (layer,momlayer,reslayer) in zip(net.layers,momnet.layers,resnet.layers):
+  for blobnum,(blob,momblob,resblob) in enumerate(zip(layer.blobs,momlayer.blobs,reslayer.blobs)):
     if blobnum == 0:
       blob.data[:]=(1.0/math.sqrt(np.prod(blob.data.shape[1:])))*np.random.standard_normal(size=blob.data.shape)
     else:
       blob.data[:]=0
     momblob.data[:]=0
+    resblob.data[:]=0
+
 
 #-------------------------------------------------
 # iterate
@@ -111,10 +137,18 @@ for passes in range(60):
 
     net.backward()
 
-    for (layer,momlayer) in zip(net.layers,momnet.layers):
-      for (blob,momblob) in zip(layer.blobs,momlayer.blobs): 
+    for (layer,momlayer,reslayer) in zip(net.layers,momnet.layers,resnet.layers):
+      for (blob,momblob,resblob) in zip(layer.blobs,momlayer.blobs,reslayer.blobs): 
         momblob.data[:]*=alpha
-        momblob.data[:]+=eta*blob.diff
+
+        if len(blob.diff.shape) == 2:
+          resblob.data[:]+=blob.diff
+          #u,s,v=scipy.sparse.linalg.svds(resblob.data,1,tol=1e-4,maxiter=1000)
+          u,s,v=mysvds(resblob.data,1)
+          momblob.data[:]+=eta*s*np.outer(u,v)
+          resblob.data[:]-=s*np.outer(u,v)
+        else:
+          momblob.data[:]+=eta*blob.diff
         blob.data[:]-=momblob.data
         blob.data[:]*=(1.0-weightdecay*eta)
 
@@ -131,39 +165,38 @@ for passes in range(60):
 now=time.time() 
 print "%7s\t%7.3f\t%7.3f\t%7s\t%4u\t%9.3e"%(nicetime(float(now-start)),sumloss/numupdates,sumsinceloss/numsinceupdates,nicecount(numupdates*batchsize),passes,eta) 
 
-netfilename=os.path.join(os.path.dirname(os.path.realpath(__file__)),'train.model')
+netfilename=os.path.join(os.path.dirname(os.path.realpath(__file__)),'trainlowrank.model')
 net.save(netfilename)
 
 # delta t average   since example pass     learning
 #            loss    last counter  num         rate
-#    40ms   0.047   0.047      64    0    1.000e-06
-#    67ms   0.070   0.094     128    0    9.999e-07
-#   120ms   0.090   0.109     256    0    9.997e-07
-#   217ms   0.078   0.066     512    0    9.994e-07
-#   438ms   0.075   0.072    1024    0    9.987e-07
-#   836ms   0.077   0.079    2048    0    9.972e-07
-#  1.659s   0.087   0.097    4096    0    9.943e-07
-#  3.336s   0.155   0.224    8192    0    9.886e-07
-#  6.670s   0.319   0.483     16K    0    9.773e-07
-# 13.364s   0.533   0.747     32K    0    9.552e-07
-# 27.332s   0.680   0.826     65K    1    9.125e-07
-# 55.103s   0.752   0.824    131K    2    8.334e-07
-#  1.846m   0.763   0.774    262K    4    6.976e-07
-#  3.701m   0.831   0.898    524K    8    4.967e-07
-#  7.389m   0.891   0.951   1048K   17    2.749e-07
-# 14.772m   0.934   0.978   2097K   34    1.340e-07
-# 25.412m   0.958   0.990   3598K   59    1.033e-07
+#   190ms   0.047   0.047      64    0    1.000e-06
+#   516ms   0.070   0.094     128    0    9.999e-07
+#  1.120s   0.090   0.109     256    0    9.997e-07
+#  2.447s   0.078   0.066     512    0    9.994e-07
+#  4.608s   0.075   0.072    1024    0    9.987e-07
+#  9.480s   0.077   0.078    2048    0    9.972e-07
+# 19.339s   0.084   0.091    4096    0    9.943e-07
+# 37.113s   0.148   0.211    8192    0    9.886e-07
+#  1.167m   0.306   0.464     16K    0    9.773e-07
+#  2.370m   0.522   0.737     32K    0    9.552e-07
+#  4.802m   0.670   0.819     65K    1    9.125e-07
+#  9.691m   0.728   0.787    131K    2    8.334e-07
+# 19.447m   0.748   0.767    262K    4    6.976e-07
+# 39.169m   0.818   0.889    524K    8    4.967e-07
+#  1.144h   0.876   0.934   1048K   17    2.749e-07
+#  1.847h   0.920   0.963   2097K   34    1.340e-07
+#  2.849h   0.943   0.977   3598K   59    1.033e-07
 
-# make test
+# make testlowrank
 # delta t average   since example pass     learning
 #            loss    last counter  num         rate
 #    15ms   0.953   0.953      64    0    1.000e-06
-#    28ms   0.961   0.969     128    0    1.000e-06
-#    54ms   0.957   0.953     256    0    1.000e-06
-#   107ms   0.967   0.977     512    0    1.000e-06
-#   227ms   0.972   0.977    1024    0    1.000e-06
-#   421ms   0.974   0.976    2048    0    1.000e-06
-#   814ms   0.968   0.963    4096    0    1.000e-06
-#  1.611s   0.976   0.984    8192    0    1.000e-06
-#  1.927s   0.978   0.988    9984    0    1.000e-06
-
+#    29ms   0.953   0.953     128    0    1.000e-06
+#    55ms   0.965   0.977     256    0    1.000e-06
+#   104ms   0.959   0.953     512    0    1.000e-06
+#   205ms   0.964   0.969    1024    0    1.000e-06
+#   383ms   0.958   0.951    2048    0    1.000e-06
+#   756ms   0.955   0.952    4096    0    1.000e-06
+#  1.584s   0.963   0.971    8192    0    1.000e-06
+#  1.999s   0.966   0.982    9984    0    1.000e-06
