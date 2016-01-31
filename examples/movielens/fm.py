8d1676e20c66fe586e1df31fe3b8d751f8e42af7
Paul Mineiro <paul.mineiro@gmail.com>
2016-01-31 08:16:00 -0800

    movielens

--- examples/movielens/fm.py
+++ examples/movielens/fm.py
@@ -40,6 +40,7 @@ protofile = os.path.join (os.path.dirname (os.path.realpath (__file__)), protofi
 with open (protofile, 'w') as f:
   f.write (str (spec))
 
+caffe.set_mode_gpu ()
 net = caffe.Net(protofile, caffe.TRAIN)
 
 for name, layer in zip (net._layer_names, net.layers):
@@ -137,3 +138,17 @@ print "%7s\t%7.3f\t%7.3f\t%7.3f\t%7s\t%4u\t%9.3e"% (
     nicecount (numupdates*batchsize),
     passes,
     eta)
+
+#GLOG_minloglevel=5 PYTHONPATH=../../python python ./fm.py                         delta average   since     acc example pass     learning
+#      t    loss    last   since counter  num         rate
+#   84ms   1.609   1.609  19.100    1000    0    9.991e-04
+#  128ms   1.609   1.609  20.500    2000    0    9.982e-04
+#  214ms   1.609   1.609  21.850    4000    0    9.964e-04
+#  387ms   1.609   1.608  22.275    8000    0    9.928e-04
+#  732ms   1.607   1.605  26.550     16K    0    9.857e-04
+# 1.402s   1.600   1.593  31.988     32K    0    9.716e-04
+# 2.589s   1.576   1.553  35.181     64K    0    9.442e-04
+# 5.238s   1.528   1.479  35.139    128K    0    8.918e-04
+#10.281s   1.533   1.538  34.515    256K    0    7.966e-04
+#20.974s   1.523   1.512  32.095    512K    0    6.392e-04
+#39.976s   1.509   1.496  31.712   1000K    0    4.309e-04
